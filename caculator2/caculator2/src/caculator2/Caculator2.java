8fac57fec5e582a7e2cd15c47dd4120e26471051
Tiffany ( thuy trang ) <132757353+thuytrang3105@users.noreply.github.com>
2024-02-19 14:58:07 +0700

    Add files via upload

---
+++ caculator2/caculator2/src/caculator2/Caculator2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caculator2;
+
+/**
+ *
+ * @author 84353
+ */
+public class Caculator2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new NewJFrame().setVisible(true);
+    }
+    
+}
